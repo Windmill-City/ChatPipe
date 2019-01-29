1af1133d1343f2348d918f604fc2ad866bf07253
Windmill_City <1449182174@qq.com>
2019-01-29 21:52:35 +0800

    Fix Minecraft's pinghandler close channel

--- src/main/java/cappcraft/chat/network/HeaderChecker.java
+++ src/main/java/cappcraft/chat/network/HeaderChecker.java
@@ -1,17 +1,19 @@
 package cappcraft.chat.network;
 
+import cappcraft.chat.Config;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufProcessor;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.channel.ChannelInitializer;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
 
 @ChannelHandler.Sharable
 public class HeaderChecker extends ChannelInboundHandlerAdapter {
     private final StringBuilder seq = new StringBuilder(3);
     private final HeaderParser parser = new HeaderParser(seq, 3);
-    private final ChannelInitializer childhandler = new ChatChannelInitializer();
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
@@ -20,17 +22,16 @@ public class HeaderChecker extends ChannelInboundHandlerAdapter {
             ByteBuf byteBuf = (ByteBuf) msg;
             if(byteBuf.forEachByte(parser) != -1 && seq.toString().equals("GET")) {
                 ctx.channel().pipeline().remove("timeout");
-                ctx.channel().pipeline().remove("legacy_query");
                 ctx.channel().pipeline().remove("splitter");
                 ctx.channel().pipeline().remove("decoder");
                 ctx.channel().pipeline().remove("prepender");
                 ctx.channel().pipeline().remove("encoder");
                 ctx.channel().pipeline().remove("packet_handler");
-                ctx.channel().pipeline().addFirst(childhandler).fireChannelActive();
+                ctx.channel().pipeline().addLast(new HttpServerCodec()).addLast(new HttpObjectAggregator(128)).addLast(new WebSocketServerProtocolHandler(Config.websocketPath)).addLast(ExternelChatHandler.INSTANCE);
             }
             parser.reset();
         }
-        ctx.fireChannelRead(msg);
+        ctx.fireChannelActive().fireChannelRead(msg);
     }
 
     private class HeaderParser implements ByteBufProcessor {
@@ -56,5 +57,9 @@ public class HeaderChecker extends ChannelInboundHandlerAdapter {
             seq.delete(0,maxLength);
             size = 0;
         }
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
     }
 }
